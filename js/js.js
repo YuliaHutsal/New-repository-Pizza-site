251b0e89686df33464874f39e0db97580d999621
Yulia Gutsal <julik.ua@gmail.com>
2022-11-15 15:32:26 +0200

    footer create

---
+++ js/js.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", init, false);
+function init(){
+    let canvas = document.getElementById('load', init, false);
+    function init(){
+        let canvas = document.getElementById('canvas');
+        let context = canvas.getContext('2d');
+        context.fillRect()
+    }
+}
