0d2caef604be743349defedcf661134ae81b671d
Yulia Gutsal <julik.ua@gmail.com>
2022-11-22 16:53:27 +0200

    jsvalidation finish

--- js/jsvalidator.js
+++ js/jsvalidator.js
@@ -1,9 +1,34 @@
-const form = document.forms[0];
-form.addEventListener("submit", function(e)
-{
-    if((form.firstName.value.lenght == 0) || (form.lastName.value.lenght == 0)){
+var form = document.forms[0];
+form.firstName.addEventListener('change', function(){
+    validateElement(form.firstName, '#firstnameError');
+});
+
+form.lastName.addEventListener('change', function(){
+    validateElement(form.lastName, '#lastnameError');
+});
+
+form.addEventListener('submit', function(e){
+    validateElement(form.firstName, '#firstnameError');
+    validateElement(form.lastName, '#lastnameError');
+    
+    let invalidElements = document.querySelectorAll('.invalid');
+    if(invalidElements.length > 0){
         e.preventDefault();
     }
-
-    form.firstName.focus();
 });
+
+function validateElement(element, errorElementSelector){
+    let errorElement = document.querySelector(
+        errorElementSelector);
+    element.classList.remove('valid');
+    element.classList.remove('invalid');
+    errorElement.style.display = "none";
+
+    if((element.value.length > 0) && (!element.validity.patternMismatch)){
+        element.classList.add('valid');
+    }
+    else{
+        element.classList.add('invalid');
+        errorElement.style.display = "inline";
+    }
+}
