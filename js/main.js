03c8dbffe2a4c83c4b788139c2604b7e9192ddfa
Yulia Gutsal <julik.ua@gmail.com>
2022-11-29 20:39:42 +0200

    media index.html

--- js/main.js
+++ js/main.js
@@ -14,9 +14,9 @@
 (function(){
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header_nav');
-    const menuCloseItem = document.querySelector('.header_nav_close')
+    const menuCloseItem = document.querySelector('.header_nav-close')
     burgerItem.addEventListener('click',() =>{
-        menu.classList.add('.header_nav_active')
+        menu.classList.add('header_nav_active')
     });
     menuCloseItem.addEventListener('click',() =>{
         menu.classList.remove('header_nav_active');
