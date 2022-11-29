15c5d71b91c117a1f642e27da07dda6712b1e42d
Yulia Gutsal <julik.ua@gmail.com>
2022-11-29 21:11:39 +0200

    delete file js.js

--- js/js.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", init, false);
-function init(){
-    let canvas = document.getElementById('load', init, false);
-    function init(){
-        let canvas = document.getElementById('canvas');
-        let context = canvas.getContext('2d');
-        context.fillRect()
-    }
-}
